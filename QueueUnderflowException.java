588fdc2eff5e950e80874ce335d2b7338feebe03
danibkatz <danielbarrykatz@gmail.com>
2013-08-06 22:01:39 -0400

    Create QueueUnderflowException.java

---
+++ QueueUnderflowException.java
@@ -0,0 +1,14 @@
+
+package graphproj;
+public class QueueUnderflowException extends QueueException
+{
+    public QueueUnderflowException()
+    {
+         super("Queue Underflow");
+    }
+
+    public QueueUnderflowException(String msg)
+    {
+        super(msg);
+    }
+}
