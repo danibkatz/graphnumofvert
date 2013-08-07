e2ebd2e6a6edc317a6c109150ea425cc31b00100
danibkatz <danielbarrykatz@gmail.com>
2013-08-06 22:01:17 -0400

    Create QueueException.java

---
+++ QueueException.java
@@ -0,0 +1,15 @@
+
+package graphproj;
+public class QueueException extends RuntimeException
+{
+    public QueueException()
+    {
+    }
+
+    public QueueException(String msg)
+    {
+        super(msg);
+    }
+}
+
+
