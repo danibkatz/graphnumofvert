398bd96e9b6caad71e775acaf7c47abf86d6b3e2
danibkatz <danielbarrykatz@gmail.com>
2013-08-06 22:02:03 -0400

    Create QueueOverflowException.java

---
+++ QueueOverflowException.java
@@ -0,0 +1,16 @@
+
+package graphproj;
+public class QueueOverflowException extends QueueException
+{
+    public QueueOverflowException()
+    {
+         super("Queue Overflow");
+    }
+
+    public QueueOverflowException(String msg)
+    {
+        super(msg);
+    }
+}
+
+
